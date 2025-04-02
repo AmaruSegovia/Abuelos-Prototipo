fc30510d971dc06a789b39240c91238e5183c6b0
carvlc <cnsvilca@gmail.com>
2025-04-02 16:35:47 -0300

    update: primer avance ciclo dia noche

---
+++ Assets/Scripts/ControllerDayNightCycle.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ControllerDayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Light2D generalLight;
+    [SerializeField] private DayNightCycle[] dayNightCycle;
+    [SerializeField] private float timePerCycle; // en segundos
+    private float currentCycleTime = 0;
+    private float percentageCycle;
+    private int currentCycle = 0;
+    private int nextCycle = 1;
+
+    private void Start()
+    {
+        generalLight.color = dayNightCycle[0].colorCycle;
+    }
+
+    private void Update()
+    {
+        currentCycleTime += Time.deltaTime;
+        percentageCycle = currentCycleTime / timePerCycle;
+
+        if (currentCycleTime >= timePerCycle)
+        {
+            currentCycleTime = 0;
+            currentCycle = nextCycle;
+
+            if (nextCycle + 1 > dayNightCycle.Length - 1 )
+            {
+                nextCycle = 0;
+            }
+            else
+            {
+                nextCycle += 1;
+            }
+        }
+
+        // cambiar el color
+        cambiarColor(dayNightCycle[currentCycle].colorCycle, dayNightCycle[nextCycle].colorCycle);
+    }
+
+    private void cambiarColor(Color colorActual, Color siguienteColor)
+    {
+        generalLight.color = Color.Lerp(colorActual, siguienteColor, percentageCycle);
+    }
+}
---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class DayNightCycle
+{
+    public Color colorCycle;
+    public string nameCycle;
+}
